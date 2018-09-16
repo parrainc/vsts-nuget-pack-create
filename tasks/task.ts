514555c83f3d25f2798bcbb82680921d64555e74
Carlos Parra <carlos.parra.sbr@gmail.com>
2018-09-15 23:54:20 -0400

    Updated task.ts and task.json

--- tasks/task.ts
+++ tasks/task.ts
@@ -3,6 +3,10 @@ import tl = require('vsts-task-lib/task');
 import trm = require('vsts-task-lib/toolrunner');
 import { isNullOrUndefined } from 'util';
 import fs = require('fs');
+import * as xml2js from 'xml2js';
+
+const nuspecExt: string = '.nuspec';
+const assemblyExt: string = '.dll';
 
 async function run() {
     try {
@@ -11,20 +15,25 @@ async function run() {
         //Get variables
         let source: string = tl.getInput('source');
         let nuspecPath: string = tl.getPathInput('nuspecPath');
-        let overrideTags: boolean = tl.getBoolInput('overridetags');
+        let overrideTags: string = tl.getInput('overridetags');
+        let tagsToOverride: string = tl.getInput('tagsToOverride');
         let assemblyPath: string = tl.getPathInput('assemblyPath');
+        let strph: string = tl.getInput('placeholder');
+        let boolph: boolean = tl.getBoolInput('testBoolean');
 
-        //make sure parameters aren't null
-        if(isNullOrUndefined(nuspecPath))
-        {
-            throw new Error("[!] Missing required input: sourcePath");
+        //make sure required parameters aren't null
+        if (overrideTags == "yes") {
+            tl.debug("Override Tags = " + overrideTags);
+            
         }
 
-        //read file
-        let specFile: string = fs.readFileSync(nuspecPath, 'utf-8');
+        console.log("source: " + source);
+        console.log("nuspec file path: " + nuspecPath);
+        console.log("override tags?: " + overrideTags);
+        console.log("tags overwritten: " + tagsToOverride);
+        console.log("assembly path: " + assemblyPath);
 
-        console.log(specFile);
-        console.log("Done!");
+        console.log("Task Done!");
     } catch (error) {
         tl.setResult(tl.TaskResult.Failed, error.message);
     }

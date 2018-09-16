fea1f9746e6f055f6fa963f60645868eaacd887f
Carlos Parra <carlos.parra.sbr@gmail.com>
2018-09-15 23:59:26 -0400

    Created function for reading nuspec file and overriding tags in it

--- tasks/task.ts
+++ tasks/task.ts
@@ -54,7 +54,7 @@ async function run() {
 
         if (overrideTags == "yes") {
             tl.debug("Override Tags = " + overrideTags);
-            
+            overrideNuspecNodes(nuspecPath, tagsToOverride);            
         }
 
         console.log("source: " + source);
@@ -67,6 +67,65 @@ async function run() {
     } catch (error) {
         tl.setResult(tl.TaskResult.Failed, error.message);
     }
+}
+
+function overrideNuspecNodes(nuspecPath: string, tagsToOverride: string) {
+    console.log("Overriding tags in nuspec file");
+    
+    tl.debug("Overriding nuspec tags");
+    
+    let nodesValues = 
+    {
+        "authors": "Carlos Parra",
+        "description": "This is the new description"
+    } //tagsToOverride.substr(1, tagsToOverride.indexOf('"')-1).trim()
+    
+    try {
+        const parser = new xml2js.Parser();
+        const builder = new xml2js.Builder();
+        let newXml: string;
+        let nuspecFile: string = readNuspecFile(nuspecPath);
+
+        parser.parseString(nuspecFile, function (err, result) {
+            if (err) console.log(err);
+
+            tl.debug("Original .nuspec file: " + result);
+            
+            let json = result;
+
+            Object.keys(nodesValues).forEach(key => {
+                let hasProperty: boolean = json.package.metadata[0].hasOwnProperty(key);
+                console.log(key);
+                console.log(hasProperty);
+                
+                tl.debug(`Old value for ${key}: ` + json.package.metadata[0][key]);
+
+                if (hasProperty) {
+                    json.package.metadata[0][key] = nodesValues[key];
+                } else {
+                    // create property
+                    tl.warning(`Property ${key} doesn't exists in file!`);
+                }
+
+                tl.debug(`New value for ${key}: ` + json.package.metadata[0][key]);
+            });
+
+            newXml = builder.buildObject(json);
+            
+            tl.debug("Modified .nuspec file: " + newXml);
+        });
+
+        // fs.writeFileSync('newfile.nuspec', newXml);
+    } catch (error) {
+        tl.error(error);
+    }
+
+    tl.debug("Overriding tags done!");
+    console.log("Overriding tags done!");
+}
+
+function readNuspecFile(nuspecPath: string) : string {
+    return fs.readFileSync(nuspecPath, 'utf-8');
 }
 
 function isValidFileExtension(filePath: string, fileExtension: string) : boolean {
